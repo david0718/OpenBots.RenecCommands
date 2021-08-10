855c060724b08132d6a5ff2e7fa23b8e024849d3
mariuszk <mariusz.kokocinski@lars.pl>
2021-08-10 09:30:23 +0200

    added Download Command

---
+++ RenecCommands/DownloadCommand.cs
@@ -0,0 +1,108 @@
+﻿using OpenBots.Core.Attributes.PropertyAttributes;
+using OpenBots.Core.Command;
+using OpenBots.Core.Enums;
+using OpenBots.Core.Infrastructure;
+using OpenBots.Core.Properties;
+using OpenBots.Core.Utilities.CommandUtilities;
+using OpenBots.Core.Utilities.CommonUtilities;
+
+using System;
+using System.Net;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Windows.Forms;
+using Tasks = System.Threading.Tasks;
+
+
+namespace RenecCommands
+{
+	[Serializable]
+	[Category("Renec Commands")]
+	[Description("This command download and save file.")]
+	public class DownloadCommand : ScriptCommand
+	{
+
+		
+
+		[Required]
+		[DisplayName("Download Link")]
+		[Description("Enter URI of the file to download.")]
+		[SampleUsage("\"http://rpachallenge.com/assets/ \" || vLink")]
+		[Remarks("")]
+		[Editor("ShowVariableHelper", typeof(UIAdditionalHelperType))]
+		[CompatibleTypes(new Type[] { typeof(string) })]
+		public string v_DownloadLink { get; set; }
+
+
+		[Required]
+		[DisplayName("Save Full Path")]
+		[Description("Enter full path ( with name) for save downloaded file.")]
+		[SampleUsage("\"D:\\text.txt \" || vSaveFullPath")]
+		[Remarks("")]
+		[Editor("ShowVariableHelper", typeof(UIAdditionalHelperType))]
+		[CompatibleTypes(new Type[] { typeof(string) })]
+		public string v_SaveFullPath { get; set; }
+
+
+
+		[Required]
+		[Editable(false)]
+		[DisplayName("Output Result Variable")]
+		[Description("Create a new variable or select a variable from the list.")]
+		[SampleUsage("vResult")]
+		[Remarks("New variables/arguments may be instantiated by utilizing the Ctrl+K/Ctrl+J shortcuts.")]
+		[CompatibleTypes(new Type[] { typeof(string) })]
+		public string v_Result { get; set; }
+
+		public DownloadCommand()
+		{
+			CommandName = "DownloadCommand";
+			SelectionName = "Download Command";
+			CommandEnabled = true;
+			CommandIcon = Resources.command_string;
+
+			
+		}
+
+		public async override Tasks.Task RunCommand(object sender)
+		{
+			var engine = (IAutomationEngineInstance)sender;
+
+			var downloadLink = (string)await v_DownloadLink.EvaluateCode(engine);
+			var saveFullPath = (string)await v_SaveFullPath.EvaluateCode(engine);
+			string resultData = "START";
+			try
+            {
+				resultData = "OK";
+				var wCli = new WebClient();
+				wCli.DownloadFile(downloadLink, saveFullPath);
+			}
+			catch (WebException webEx)
+			{
+				var err = webEx.ToString();
+				resultData = err;
+			}
+		
+
+			resultData.SetVariableValue(engine, v_Result);
+		}
+
+		public override List<Control> Render(IfrmCommandEditor editor, ICommandControls commandControls)
+		{
+			base.Render(editor, commandControls);
+
+	
+			RenderedControls.AddRange(commandControls.CreateDefaultInputGroupFor("v_DownloadLink", this, editor));
+			RenderedControls.AddRange(commandControls.CreateDefaultInputGroupFor("v_SaveFullPath", this, editor));
+			RenderedControls.AddRange(commandControls.CreateDefaultOutputGroupFor("v_Result", this, editor));
+
+			return RenderedControls;
+		}
+
+		public override string GetDisplayValue()
+		{
+			return base.GetDisplayValue() + $" [Get '{v_DownloadLink}' - Store File in '{v_SaveFullPath}']";
+		}
+	}
+}
--- RenecCommands/Properties/AssemblyInfo.cs
+++ RenecCommands/Properties/AssemblyInfo.cs
@@ -5,10 +5,10 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("RenecCommands")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("RenecCommands for OpenBots Studio")]
+[assembly: AssemblyDescription("Custom Commands for OpenBots Studio")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Renecsoft")]
 [assembly: AssemblyProduct("RenecCommands")]
 [assembly: AssemblyCopyright("Copyright ©  2021")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.1.0")]
+[assembly: AssemblyFileVersion("1.0.1.0")]
